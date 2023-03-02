08a52a102bff23747f3341f9433d552fc8680841
dim <ilona.demchyshyna@gmail.com>
2023-03-02 17:53:36 +0200

    tasks

---
+++ task.js
@@ -0,0 +1,53 @@
+
+function firstTask() {
+
+    for(let i = 5; i < 11; i++)
+    { console.log(i);
+    }
+}
+
+function secondTask() {
+
+    for(let i = 20; i >= 10; i--) {
+        if(i===13)break;
+        console.log(i);
+    }
+
+}
+
+function thirdTask() {
+
+    for(let i = 2; i <= 10; i++) {
+        if(i % 2 === 0) {
+            console.log(i);
+
+        }
+    }
+
+}
+
+
+function fourthTask() {
+
+    let i = 2;
+    while(i <= 16) {
+        if(i % 2 === 0) { i++; continue; }
+        else { console.log(i); }
+        i++;
+    }
+
+}
+
+
+
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    for(let i = 5; i < 11; i++)
+    { arrayOfNumbers[i - 5] = i;
+    }
+    console.log(arrayOfNumbers);
+
+    return arrayOfNumbers;
+}
---
+++ triangle.js
@@ -0,0 +1,9 @@
+let result = '';
+const length = 7;
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < 1; j++) {
+        result += '*';
+    }
+    result += '\n';
+}
+console.log(result);
